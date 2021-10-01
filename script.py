a3ce25aebb0b9a81e47fed5fcc3cb0deba00c452
Pradhumna Banerji <60711601+pradhumna22@users.noreply.github.com>
2021-10-01 09:05:01 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,3 @@
 
-reverseString = input("Please enter a string to be reversed: ")
-
-for i in range(len(reverseString)):
-        counter = len(reverseString) - i -1
-        print(reverseString[counter], end ="")
+reverseString = input("Please enter a string to be reversed: ")             
+        print(reverseString[::-1])
